4e5dd5bc00b8a6c5dd6da8fc48bec8795d56f173
jazylboukerdenna <jazylboukerdenna@hotmail.com>
2023-11-26 14:23:28 -0500

    test number 3

---
+++ src/main/java/org/example/dto/SchoolManagementSystem.java
@@ -0,0 +1,67 @@
+package org.example.dto;
+
+public class SchoolManagementSystem {
+
+    // Fields
+    // Add necessary fields here
+
+    // Constructor
+    public SchoolManagementSystem(String departmentName) {
+        // Implementation for constructor
+    }
+
+    // Methods (without implementation)
+    public Department findDepartment(String departmentId) {
+        // Method declaration without implementation
+
+    }
+
+    public void printTeachers() {
+        // Method declaration without implementation
+    }
+
+    public void modifyCourseTeacher(String courseId, String teacherId) {
+        // Method declaration without implementation
+    }
+
+    public void addDepartment(String departmentName) {
+        // Method declaration without implementation
+    }
+
+    public void printStudents() {
+        // Method declaration without implementation
+    }
+
+    public Student findStudent(String studentId) {
+        // Method declaration without implementation
+    }
+
+    public void addCourse(String courseId, double credit, String departmentId) {
+        // Method declaration without implementation
+    }
+
+    public void registerCourse(String studentId, String courseId) {
+        // Method declaration without implementation
+    }
+
+    public void addTeacher(String teacherId, String firstName, String lastName) {
+        // Method declaration without implementation
+    }
+
+    public Course findCourse(String courseId) {
+        // Method declaration without implementation
+    }
+
+    public void printDepartments() {
+        // Method declaration without implementation
+    }
+
+    public void addStudent(String studentId, String firstName, String lastName) {
+        // Method declaration without implementation
+    }
+
+    public Teacher findTeacher(String teacherId) {
+        // Method declaration without implementation
+    }
+}
+
