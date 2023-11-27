91c7efdbde2134ba3b63afd3714f456da5cf1019
JazylBoukerdenna <150823955+JazylBoukerdenna@users.noreply.github.com>
2023-11-26 23:59:16 -0500

    Update SchoolManagementSystem.java

--- src/main/java/org/example/dto/SchoolManagementSystem.java
+++ src/main/java/org/example/dto/SchoolManagementSystem.java
@@ -15,80 +15,64 @@ public class SchoolManagementSystem {
 
         public SchoolManagementSystem(String filePath) {
             // Constructor to initialize the SchoolManagementSystem using data from a file
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public Department findDepartment(String departmentId) {
             // Method to find and return a department based on departmentId
-            // Implementation can be added based on project requirements
-            return null;
+             return null;
         }
 
         public void printTeachers() {
             // Method to print all teachers in the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void modifyCourseTeacher(String courseId, String teacherId) {
             // Method to modify the teacher of a course based on courseId and teacherId
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void addDepartment(String departmentName) {
             // Method to add a new department to the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void printStudents() {
             // Method to print all students in the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public Student findStudent(String studentId) {
             // Method to find and return a student based on studentId
-            // Implementation can be added based on project requirements
-            return null;
+             return null;
         }
 
         public void addCourse(String courseId, double credit, String departmentId) {
             // Method to add a new course to the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void registerCourse(String studentId, String courseId) {
             // Method to register a course for a student based on studentId and courseId
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void addTeacher(String teacherId, String firstName, String lastName) {
             // Method to add a new teacher to the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public course.Course findCourse(String courseId) {
             // Method to find and return a course based on courseId
-            // Implementation can be added based on project requirements
-
-            return null;
+              return null;
         }
 
         public void printDepartments() {
             // Method to print all departments in the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public void addStudent(String studentId, String firstName, String lastName) {
             // Method to add a new student to the school
-            // Implementation can be added based on project requirements
-        }
+         }
 
         public Teacher findTeacher(String teacherId) {
             // Method to find and return a teacher based on teacherId
-            // Implementation can be added based on project requirements
-            return null;
+             return null;
         }
 
-        // Other methods as per requirements
-    }
+     }
 
 
