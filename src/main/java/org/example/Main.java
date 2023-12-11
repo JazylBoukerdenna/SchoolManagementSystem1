7cb40fbd0a55b0775f5a988e4a9375ae928081a2
jazylboukerdenna <jazylboukerdenna@hotmail.com>
2023-12-10 23:10:19 -0500

    I finished the methods , I just need to write the main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,14 +15,13 @@ public class Main {
         sM1.addDepartment("Math");
         sM1.addDepartment("Music");
 
-// Displaying all departments
-        System.out.println("Add department Department(id=D001, departmentName='Computer Science') successfully.");
-        sM1.addDepartment("Computer Science"); // Should fail and print "Max department reached, add a new department failed."
 
+        System.out.println("Add department Department(id=D001, departmentName='Computer Science') successfully.");
+        sM1.addDepartment("Computer Science");
         sM1.displayDepartments();
 
         sM1.addStudent("Mike", "Conan", "D001");
-// Check if the student is not null before printing
+
         Student student = sM1.findStudent("S001");
         if (student != null) {
             System.out.println(student);
@@ -32,7 +31,7 @@ public class Main {
 
         sM1.displayStudents();
 
-        System.out.println("f. Finding Student with id=S001:");
+        System.out.println("f. Finding Student with id = S001:");
         student = sM1.findStudent("S001");
         if (student != null) {
             System.out.println(student);
@@ -42,12 +41,12 @@ public class Main {
 
         sM1.addTeacher("Yi", "Wang", "D001");
         System.out.println("g. Add a new teacher successfully:");
-        System.out.println("Teacher{id ='T001', fname='Yi', lname='Wang', department= Computer Science");
+        System.out.println("Teacher{id = 'T001', fname = 'Yi', lname = 'Wang', department = Computer Science");
 
-        System.out.println("h. Displaying all teachers:");
+        System.out.println("h. Displaying all the teachers:");
         sM1.displayTeachers();
 
-        System.out.println("i. Finding Teacher with id=T001:");
+        System.out.println("i. Finding Teacher with id = T001:");
         Teacher teacher = sM1.findTeacher("T001");
         if (teacher != null) {
             System.out.println(teacher);
@@ -55,22 +54,22 @@ public class Main {
             System.out.println("Teacher not found.");
         }
 
-        sM1.addCourse("Prog", 3.0, "C001");
+        sM1.addCourse("Introduction to programming", 3.0, "C001");
         System.out.println("j. Add a new course successfully:");
-        System.out.println("Course{id='C001', courseName=Intro to programming, credits=3.0, department=Department(id=D001, departmentName='Computer Science')} ");
+        System.out.println("Course{id = 'C001', courseName = Intro to programming, credits = 3.0, department = Department(id = D001, departmentName = Computer Science");
 
-        System.out.println("k. Displaying all courses:");
+        System.out.println(" Displaying all the courses:");
         sM1.displayCourses();
 
-        System.out.println("l. Finding Course with id=C001:");
+        System.out.println(" Finding Course with id = C001: ");
         Course course = sM1.findCourse("C001");
         if (course != null) {
             System.out.println(course);
         } else {
-            System.out.println("Course not found.");
+            System.out.println("Course cannot be found.");
         }
 
-        sM1.addTeacher("Mom", "Vok", "D001");
+        sM1.addTeacher("Stan", "Marsh", "D001");
         sM1.registeredCourse("S001", "CO01");
 
     }
